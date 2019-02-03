5d3450495a7e276d5fbc796f4e9436f2be6289ee
Jedai Saboteur <dev@paradoxinversion.com>
2019-02-03 14:10:25 -0800

    Made heroko baseurl https

--- app/axiosInstance.js
+++ app/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const baseURL = window.location.host.includes("localhost")
   ? "http://localhost:3000/api"
-  : "http://paradox-inversion.herokuapp.com/api";
+  : "https://paradox-inversion.herokuapp.com/api";
 const axiosInstance = axios.create({
   baseURL
 });
