5d5267c059824280264fd3ee5abe8e9805170f73
Jedai Saboteur <dev@paradoxinversion.com>
2019-02-14 22:39:41 -0800

    Changed axiosInstance, began departure from heroku

--- app/axiosInstance.js
+++ app/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-const baseURL = window.location.host.includes("localhost")
-  ? "http://localhost:3000/api"
-  : "https://paradox-inversion.herokuapp.com/api";
+// change to environment variable
+const baseURL = "http://localhost:3000/api";
 const axiosInstance = axios.create({
   baseURL
 });
