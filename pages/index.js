eafc7f59132de45b28ddd470bd6fd004d7945364
Jedai Saboteur <dev@paradoxinversion.com>
2019-08-11 16:51:00 -0700

    Added page content back to index

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import React from "react";
 import MainLayout from "../components/MainLayout";
 import PostTeaserList from "../components/PostTeaserList";
 import { NextSeo } from "next-seo";
+import Page from "./[slug]";
 
 // import "../style.css";
 class Index extends React.Component {
@@ -33,7 +34,13 @@ class Index extends React.Component {
                 "Home of Fiction, Articles, and Games by Jedai Saboteur"
             }}
           />
+
           <div className="margin--standard">
+            <div
+              dangerouslySetInnerHTML={{
+                __html: this.props.pages[0].content
+              }}
+            />
             <PostTeaserList searchType="category" query="announcements" />
           </div>
         </MainLayout>
